ada073040b14dabdd90c62f37e575bbcd47bdade
Jessica Kellogg <jmy.kellogg@gmail.com>
2016-07-29 09:51:52 -0500

    starting merge

--- mergesort.js
+++ mergesort.js
@@ -1,8 +1,35 @@
 function split (array){
-	var halfLenght= Math.floor(array.length/2);
+	var halfLength= Math.floor(array.length/2);
 
-	var firstArray = array.slice(0, halfLenght)
-	var secondArray = array.slice(halfLenght, array.length);
+	var firstArray = array.slice(0, halfLength)
+	var secondArray = array.slice(halfLength, array.length);
 
 	return  [firstArray, secondArray]
 }
+
+function merge(a,b){
+	var c = []
+	do {
+	if(a[0] > b[0]){
+		c.push(b.shift())
+	} else {
+		c.push(a.shift())
+	}
+	} while (a.length && b.length) 
+
+	if (a.length) {
+		c = c.concat(a)
+	} else {
+		c = c.concat(b)
+	}
+	return c
+}
+
+function mergeSort(array){
+
+	
+
+}
+
+
+
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -6,3 +6,25 @@ describe('Split Array function', function() {
     expect(split([1,2,3,4,5])).toEqual([[1,2],[3,4,5]])
   });
 });
+
+describe('Merge Sort', function(){
+  it('is able to merge two sorted arrays into one sorted array', function(){
+       expect(merge([1,3],[2,4])).toEqual([1,2,3,4])
+  });
+    it('should be able to merge odd number of element', function() {
+    expect(merge([1,4],[2,3,5])).toEqual([1,2,3,4,5])
+  });
+});
+
+describe('mergeSort function', function(){
+	it('is able to return an array of the same length', function(){
+		var arr1 = [1,3,5,2,4,6], sortedArr1 = [1,2,3,4,5,6];
+		expect(mergeSort(arr1).length).toEqual(6)
+	});
+	it('is able to sort an array',function(){
+		var arr1 = [1,3,5,2,4,6], sortedArr1 = [1,2,3,4,5,6];
+		expect(mergeSort(arr1)).toEqual(sortedArr1);
+	})
+
+
+});
